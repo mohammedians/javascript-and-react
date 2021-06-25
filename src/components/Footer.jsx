b0fd94586d15ce57b36cd18797c98a18d4d02e84
mohammedians <86470904+mohammedians@users.noreply.github.com>
2021-06-25 16:31:23 +0530

    Add files via upload
    Shape AI 7 days bootcamp of basic web dev with java script and react.js

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyrights by Shape AI@ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java Script And React</h1>
+      <p>
+        A bootcamp based on basic training of Web Dev with Javascript and
+        React.js
+      </p>
+    </div>
+  );
+}
+export default Info;
